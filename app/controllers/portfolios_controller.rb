9034b9bea448db51a5e1be4de31439ad21a5e7ef
abankson <adam.bankson@gmail.com>
2018-05-17 15:23:57 +0900

    Implemented index action and view for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,2 +1,9 @@
 class PortfoliosController < ApplicationController
+
+  def index #generally where a whole list is displayed
+    @portfolioItems = Portfolio.all 
+    #This is a call to the Portfolio MODEL from this CONTROLLER and assigning 
+    # the values of all Portfolio items to @portfolio_items which can be accessed by the view
+    #In order to make use of @portfolio_items a file in views must be made for portfolios index 'index.html.erb'
+  end
 end
