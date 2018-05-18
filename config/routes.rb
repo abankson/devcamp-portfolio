0cb9ea61d9553ae12823d853363de3a1a23abad6
abankson <adam.bankson@gmail.com>
2018-05-18 15:09:44 +0900

    Overrode the show route to be portfolio/:id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  #To do custom routes on resourced routes, use except then configure under
+  #except expects an array
+  resources :portfolios, except: [:show] 
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio-singular'
 
   #get 'pages/about' | Below is how you map without pages/about
   get 'about-me', to: 'pages#about' #controller on the left side, action on the right side of hash
