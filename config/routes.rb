5142343b8268a56d73536cba7e38469efed79505
abankson <adam.bankson@gmail.com>
2018-05-18 14:52:14 +0900

    Added custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/home'
-  get 'pages/about'
-  get 'pages/contact'
+
+  #get 'pages/about' | Below is how you map without pages/about
+  get 'about-me', to: 'pages#about' #controller on the left side, action on the right side of hash
+  #get 'pages/contact'
+  get 'contact', to: 'pages#contact' #'contact' could be name ANYTHING. so long as pages#contact is intact
+
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #sets root to home. Basically changing your homepage to home
+  root to: 'pages#home'
+
 end
