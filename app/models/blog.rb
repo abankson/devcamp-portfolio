756541a69fe14f967b376fa514b9b3f8fe780a9a
abankson <adam.bankson@gmail.com>
2018-05-19 13:36:02 +0900

    Added draft and published enums to Blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,7 @@
 class Blog < ApplicationRecord
+  #Enumeration for status of blogs. Stored as 1 and 0 but really represents draft and published
+  enum status: { draft: 0, published: 1 }
+
   extend FriendlyId
   friendly_id :title, use: :slugged
 end
